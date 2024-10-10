28ce60a7e2c79d45d8234a4f3e5ca83c49e2b0ee
Dieter Agten <dieter.agten@student.ucll.be>
2024-10-10 13:46:13 +0200

    OO - Operator overloading OK

---
+++ 02-oo/03-properties/04-length-converter/student.py
@@ -0,0 +1,27 @@
+class LenghtConverter:
+    FEET_PER_METER = 3.28084
+    INCH_PER_METER = 39.3701
+
+    def __init__(self):
+        self.__length_in_meter = 0
+
+    @property
+    def meter(self):
+        return self.__length_in_meter
+    @meter.setter
+    def meter(self, value):
+        self.__length_in_meter = value
+    
+    @property
+    def feet(self):
+        return self.__length_in_meter * LenghtConverter.FEET_PER_METER
+    @feet.setter
+    def feet(self, value):
+        self.__length_in_meter = value / LenghtConverter.FEET_PER_METER
+
+    @property
+    def inch(self):
+        return self.__length_in_meter * LenghtConverter.INCH_PER_METER
+    @inch.setter
+    def inch(self, value):
+        self.__length_in_meter = value / LenghtConverter.INCH_PER_METER
---
+++ 02-oo/04-operator-overloading/01-money/student.py
@@ -0,0 +1,18 @@
+class Money:
+    def __init__(self, amount, currency):
+        self.amount = amount
+        self.currency = currency
+    
+    def __add__(self, other):
+        if self.currency != other.currency:
+            raise RuntimeError("Mismatched currencies!")
+        return Money(self.amount + other.amount, self.currency)
+    
+    def __sub__(self, other):
+        if self.currency !=  other.currency:
+            raise RuntimeError("Mismatched currencies!")
+        return Money(self.amount - other.amount, self.currency)
+            
+    def __mul__(self, value):
+        self.amount *= value
+        return Money(self.amount, self.currency)
