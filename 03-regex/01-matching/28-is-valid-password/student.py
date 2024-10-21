0dbad85826577d6bc79b2627f4bb33c25bd463f8
Dieter Agten <dieter.agten@student.ucll.be>
2024-10-21 23:25:01 +0200

    Regex - 1 Matching OK

--- 03-regex/01-matching/28-is-valid-password/student.py
+++ 03-regex/01-matching/28-is-valid-password/student.py
@@ -1,1 +1,25 @@
-# Write your code here
+import re
+
+def is_valid_password(string):
+    positiveregex = [
+        r".{12,}",
+        r"[a-z]",
+        r"[A-Z]",
+        r"[0-9]",
+        r"[-+/.*@]",
+    ]
+
+    negativeregex = [
+        r"(.)\1{2}",
+        r"(.)(.*\1){3}",
+    ]
+
+    for regex in positiveregex:
+        if not re.search(regex, string):
+            return False
+    
+    for regex in negativeregex:
+        if re.search(regex, string):
+            return False
+    
+    return True
