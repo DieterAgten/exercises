919f39ca9d6d3e63800434102cada16a89f57186
Dieter Agten <dieter.agten@student.ucll.be>
2024-10-22 20:09:31 +0200

    Regex - Substitutions OK

--- 03-regex/03-substituting/01-remove-trailing-whitespace/student.py
+++ 03-regex/03-substituting/01-remove-trailing-whitespace/student.py
@@ -1,1 +1,11 @@
-# Write your code here
+import re
+
+def remove_trailing_whitespace(string):
+    return re.sub(r'\s+$', '', string, flags = re.MULTILINE)
+
+"""
+\s = witruimte (spatie, tab, nieuwe regels)
++ = 1 of meerdere keren witruimte
+$ = vanaf het einde string
+flags = re.MULTILINE = Regex op het einde van elke regel ipv op het einde van de string
+"""
--- 03-regex/03-substituting/02-remove-repeated-words/student.py
+++ 03-regex/03-substituting/02-remove-repeated-words/student.py
@@ -1,1 +1,11 @@
-# Write your code here
+import re
+
+def remove_repeated_words(string):
+    return re.sub(r'(\b[a-zA-Z]+\b)( \1)+', r'\1', string)
+
+"""
+\b = woordgrens
+(...) zet woord in een groep (1 of meerdere letters)
+( \1) = Herhaald het woord, voorafgegaan door een spatie
+r'\1' = De dubbele woorden worden vervangen door 1 woord, voorafgegaan door een spatie
+"""
